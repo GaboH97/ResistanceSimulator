91c1c54a6c7e612bbcd56e412998a0c76758f1f6
kennedy1-7 <kennedy1-7@192.168.1.6>
2017-03-06 22:19:14 -0500

    Modified Resistance Methods. Test and GUI missing.

--- src/models/entity/Resistance.java
+++ src/models/entity/Resistance.java
@@ -6,8 +6,7 @@ package models.entity;
  */
 public class Resistance {
 
-    private byte tolerancePercentage;
-    private double multiplicator;
+    private float tolerancePercentage;
     private byte bandAmount;
     private double value;
     private int ppm;
@@ -17,7 +16,6 @@ public class Resistance {
         bandAmount = 0;
         value = 0;
         ppm = 0;
-        multiplicator = 0;
     }
 
     /**
@@ -25,23 +23,139 @@ public class Resistance {
      * @param colorCodes
      * @return The value of the resistance
      */
-    public double calculateValue(String colorCodes) {
-        switch (bandAmount) {
-            case 4:
-                //value = ((firstBand*10)+secondBand)*multiplicator;
-                break;
-            case 5:
-                ////value = ((firstBand*100)+secondBand*10)*multiplicator;
-                break;
-            case 6:
-                //value = (((firstBand*1000)+secondBand*100)+thirdBand*10)*multiplicator;
-                break;
+    public double calculateValue(BandColor[] bandColors) {
+        if (bandAmount == 4) {
+            value = ((getColorValue(bandColors[0]) * 10) + getColorValue(bandColors[1])) * getColorMultiplicator(bandColors[2]);
+            tolerancePercentage = getTolerancePercentage(bandColors[3]);
+        } else {
+            value = ((getColorValue(bandColors[0]) * 100) + (getColorValue(bandColors[1]) * 10) + (getColorValue(bandColors[2]))) * getColorMultiplicator(bandColors[3]);
+            tolerancePercentage = getTolerancePercentage(bandColors[3]);
+            if (bandAmount == 6) {
+                ppm = getPPMValue(bandColors[5]);
+            }
         }
         return value;
     }
 
-    public byte getTolerancePercentage() {
+    public byte getPPMValue(BandColor bandColor) {
+        byte ppmValue = 0;
+        switch (bandColor) {
+            case PURPLE:
+                ppmValue = 5;
+                break;
+            case BLUE:
+                ppmValue = 10;
+                break;
+            case ORANGE:
+                ppmValue = 15;
+                break;
+            case YELLOW:
+                ppmValue = 25;
+                break;
+            case RED:
+                ppmValue = 50;
+                break;
+            case BROWN:
+                ppmValue = 100;
+                break;
+        }
+        return ppmValue;
+    }
+
+    public float getTolerancePercentage(BandColor bandColor) {
+        float tolerance = 0;
+        switch (bandColor) {
+            case GOLDEN:
+                tolerance = 0.05f;
+                break;
+            case SILVER:
+                tolerance = 0.1f;
+                break;
+            case RED:
+                tolerance =  0.2f;
+                break;
+            case BROWN:
+                tolerance =  0.01f;
+                break;
+            default:
+        }
         return tolerancePercentage;
+    }
+
+    public float getTolerancePercentage() {
+        return tolerancePercentage;
+    }
+
+    public double getColorMultiplicator(BandColor bandColor) {
+        double multiplicator = 0;
+        switch (bandColor) {
+            case BLACK:
+                multiplicator = 1;
+                break;
+            case BROWN:
+                multiplicator = 10;
+                break;
+            case RED:
+                multiplicator = 100;
+                break;
+            case ORANGE:
+                multiplicator = 1000;
+                break;
+            case YELLOW:
+                multiplicator = 10000;
+                break;
+            case GREEN:
+                multiplicator = 100000;
+                break;
+            case BLUE:
+                multiplicator = 1000000;
+                break;
+            case PURPLE:
+                multiplicator = 0.1;
+                break;
+            case GRAY:
+                multiplicator = 0.01;
+                break;
+            default:
+                break;
+        }
+        return multiplicator;
+    }
+
+    public byte getColorValue(BandColor bandColor) {
+        byte colorValue;
+        switch (bandColor) {
+            case BROWN:
+                colorValue = 1;
+                break;
+            case RED:
+                colorValue = 2;
+                break;
+            case ORANGE:
+                colorValue = 3;
+                break;
+            case YELLOW:
+                colorValue = 4;
+                break;
+            case GREEN:
+                colorValue = 5;
+                break;
+            case BLUE:
+                colorValue = 6;
+                break;
+            case PURPLE:
+                colorValue = 7;
+                break;
+            case GRAY:
+                colorValue = 8;
+                break;
+            case WHITE:
+                colorValue = 9;
+                break;
+            default:
+                colorValue = 0;
+        }
+        return colorValue;
     }
 
     public void setTolerancePercentage(byte tolerancePercentage) {
